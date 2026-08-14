f3aa35f7c4af0483046c6039c4f14e37f795273f
jvbrowerx-cpu <jvbrowerx@gmail.com>
2026-08-14 15:35:34 -0400

    Add files via upload

---
+++ pipeline/carcinos_ingestion/triage/__init__.py
@@ -0,0 +1,15 @@
+from .openai_client import OpenAIClient, StructuredResult
+from .pass1 import run_pass1, enforce_pass1_keep_rules, Pass1Result
+from .pass2 import run_pass2, decide_notify, verify_evidence_quotes, Pass2Result
+from .tier_logic import compute_tier, tier_rationale
+from .context_brief import generate_context_brief
+from . import schemas
+
+__all__ = [
+    "OpenAIClient", "StructuredResult",
+    "run_pass1", "enforce_pass1_keep_rules", "Pass1Result",
+    "run_pass2", "decide_notify", "verify_evidence_quotes", "Pass2Result",
+    "compute_tier", "tier_rationale",
+    "generate_context_brief",
+    "schemas",
+]
